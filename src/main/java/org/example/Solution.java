677785f63fb9563d618d84f77d6fb71e89c486e3
alek <wait82985@gmail.com>
2023-06-09 12:03:38 +0600

    problem solved. binary search

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -2,7 +2,34 @@ package org.example;
 
 public class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        //TODO
-        return 0;
+
+        int index = binarySearch(letters, target);
+        if (index > letters.length - 1) {
+            return letters[0];
+        } else if (letters[index] == target) {
+            while (letters[index] == target && index < letters.length - 1) {
+                index++;
+            }
+            return letters[index] == target ? letters[0] : letters[index];
+        } else return letters[index];
+    }
+
+    public int binarySearch(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length - 1;
+        int mid = left + (right - left) / 2;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (letters[mid] == target) {
+                return mid;
+            }
+            if (target < letters[mid]) {
+                right = mid - 1;
+            }
+            if (target > letters[mid]) {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -22,8 +22,24 @@ public class SolutionTest {
     @Test
     public void nextGreatestLetterTest3() {
         char[] letters = {'x', 'x', 'y', 'y'};
-        char target = 'c';
-        char output = 'f';
+        char target = 'z';
+        char output = 'x';
         Assert.assertEquals(output, new Solution().nextGreatestLetter(letters, target));
+    }
+
+    @Test
+    public void nextGreatestLetterTest4() {
+        char[] letters = {'c', 'f', 'j', 'm', 'n', 'p', 'q', 's', 'z'};
+        char target = 'y';
+        char output = 'z';
+        Assert.assertEquals(output, new Solution().nextGreatestLetter(letters, target));
+    }
+
+    @Test
+    public void binarySearchTest1() {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+        int output = 0;
+        Assert.assertEquals(output, new Solution().binarySearch(letters, target));
     }
 }
