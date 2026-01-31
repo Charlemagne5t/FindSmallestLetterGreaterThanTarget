54dff0865055feb964028ff46c8d4887bf7c4fc9
alek <wait82985@gmail.com>
2026-01-31 19:56:36 +0500

    problem solved

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,35 +1,21 @@
 package org.example;
 
-public class Solution {
+class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-
-        int index = binarySearch(letters, target);
-        if (index > letters.length - 1) {
-            return letters[0];
-        } else if (letters[index] == target) {
-            while (letters[index] == target && index < letters.length - 1) {
-                index++;
-            }
-            return letters[index] == target ? letters[0] : letters[index];
-        } else return letters[index];
-    }
-
-    public int binarySearch(char[] letters, char target) {
-        int left = 0;
-        int right = letters.length - 1;
-        int mid = left + (right - left) / 2;
-        while (left <= right) {
-            mid = left + (right - left) / 2;
-            if (letters[mid] == target) {
-                return mid;
-            }
-            if (target < letters[mid]) {
-                right = mid - 1;
-            }
-            if (target > letters[mid]) {
-                left = mid + 1;
+        int l = 0;
+        int r = letters.length - 1;
+        int mid;
+        int res = -1;
+        while(l <= r) {
+            mid = l + ( r - l) / 2;
+            if(letters[mid] > target) {
+                r = mid - 1;
+            }else {
+                res = mid;
+                l = mid + 1;
             }
         }
-        return left;
+
+        return res == letters.length - 1 ? letters[0] : letters[res + 1];
     }
 }
